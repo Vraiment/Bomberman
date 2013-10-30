1270ead454594250875796e0e61c4be89bfa3375
Vraiment <jemc44@gmail.com>
2013-10-30 00:22:21 -0600

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Bomberman
+//
+//  Created on 30/10/13.
+//  Copyright (c) 2013 Vraiment. All rights reserved.
+//
+
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	SDL_Quit();
+	
+	return 0;
+}
