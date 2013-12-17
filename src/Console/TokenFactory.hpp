58b2e0a72020042b93a18514060f8a53647a2c5c
Vraiment <jemc44@gmail.com>
2013-12-16 21:30:48 -0600

    Renamed LexicalAnalyzer to TokenFactory, and tuned it a little.

---
+++ src/Console/TokenFactory.hpp
@@ -0,0 +1,47 @@
+//
+//  TokenFactory.hpp
+//  Bomberman
+//
+//  Created on 10/12/13.
+//  Copyright (c) 2013 Vraiment. All rights reserved.
+//
+
+#ifndef __TokenFactory__hpp__
+#define __TokenFactory__hpp__
+
+#include <string>
+
+#include "Token.hpp"
+
+namespace Bomberman {
+	class TokenFactory {
+	public:
+		TokenFactory();
+		
+		void push(std::string string);
+		
+		std::string lexeme() const;
+		
+		Token currentToken() const;
+		Token nextToken() const;
+		
+		bool empty() const;
+		void clear();
+		
+		void popToken();
+		TokenFactory& operator<<(std::string string);
+		
+	private:
+		void processToken();
+		
+		char nextSymbol();
+		void popSymbol();
+		
+		bool processing;
+		Token cToken, nToken;
+		std::string cLexeme, nLexeme;
+		std::string buffer;
+	};
+}
+
+#endif //__TokenFactory__hpp__
