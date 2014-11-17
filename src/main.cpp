eeb9ed3dc4a64304e24b2aedd0c54716c38444d6
Vraiment <jemc44@gmail.com>
2014-11-17 14:07:58 -0600

    Added exception trap

--- src/main.cpp
+++ src/main.cpp
@@ -24,28 +24,36 @@ using namespace Bomberman;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	//Standalone objects
-	Engine engine;
-	Configuration config("config.xml");
-	shared_ptr<CommandFactory> commandFactory(new CommandFactory());
-	TxtTileMapLoader mapLoader;
-	MainLoop loop;
+	try {
+		//Standalone objects
+		Engine engine;
+		Configuration config("config.xml");
+		shared_ptr<CommandFactory> commandFactory(new CommandFactory());
+		TxtTileMapLoader mapLoader;
+		MainLoop loop;
 	
-	//Dependants objects
-	shared_ptr<Viewport> viewport(new Viewport(config.viewportWidth(), config.viewportHeight(), config.viewportTitle()));
-	shared_ptr<PlayerEvents> playerEvents(new PlayerEvents(commandFactory, loop.commandQueue()));
-	shared_ptr<TileMapBuilder> builder = mapLoader.load("map1.txt");
-	shared_ptr<TileMap> tileMap(new TileMap(builder));
+		//Dependants objects
+		shared_ptr<Viewport> viewport(new Viewport(config.viewportWidth(), config.viewportHeight(), config.viewportTitle()));
+		shared_ptr<PlayerEvents> playerEvents(new PlayerEvents(commandFactory, loop.commandQueue()));
+		shared_ptr<TileMapBuilder> builder = mapLoader.load("map1.txt");
+		shared_ptr<TileMap> tileMap(new TileMap(builder));
 	
-	commandFactory->setTileMap(tileMap);
-	commandFactory->setPlayer(tileMap->player());
+		commandFactory->setTileMap(tileMap);
+		commandFactory->setPlayer(tileMap->player());
 	
-	viewport->loadTileMap(tileMap);
+		viewport->loadTileMap(tileMap);
 	
-	//Game
-	loop.addEventListener(playerEvents);
-	loop.addScreen(viewport);
-	loop.run();
+		//Game
+		loop.addEventListener(playerEvents);
+		loop.addScreen(viewport);
+		loop.run();
+	} catch (exception& e) {
+#ifdef DEBUG
+		throw e;
+#endif
+		
+		return 1;
+	}
 	
 	return 0;
 }
