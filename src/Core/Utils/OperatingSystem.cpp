807369f303f91f0d92245a2e6830a77640ebaf0f
Vraiment <jemc44@gmail.com>
2016-01-12 01:56:59 -0600

    Added OperatinSystem::listDir()

--- src/Core/Utils/OperatingSystem.cpp
+++ src/Core/Utils/OperatingSystem.cpp
@@ -8,6 +8,12 @@
 
 #include "OperatingSystem.hpp"
 
+#include <memory>
+
+#if defined(__APPLE__)
+#include <dirent.h>
+#endif
+
 using namespace std;
 
 namespace Bomberman {
@@ -23,5 +29,25 @@ namespace Bomberman {
     
     string getPath(vector<string> dirs, string file) {
         return getPath(dirs) + file;
+    }
+    
+    bool listDir(string path, vector<string>& contents) {
+        bool result = false;
+        
+#if defined(__APPLE__)
+        DIR *d = opendir(path.c_str());
+        if (nullptr != d) {
+            shared_ptr<DIR> dir(d, closedir);
+            
+            // read each entry in the directory
+            for (auto ent = readdir(dir.get()); nullptr != ent; ent = readdir(dir.get())) {
+                contents.push_back(ent->d_name);
+            }
+            
+            result = true;
+        }
+#endif
+        
+        return result;
     }
 }
